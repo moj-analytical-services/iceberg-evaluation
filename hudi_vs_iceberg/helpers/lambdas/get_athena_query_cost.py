16bcbf2bef763d12c7ce5095204b9eb14e2dac8e
David Bridgwood <david.bridgwood@digital.justice.gov.uk>
2023-05-25 15:14:11 +0100

    add lambdas to calculate costs

---
+++ hudi_vs_iceberg/helpers/lambdas/get_athena_query_cost.py
@@ -0,0 +1,77 @@
+import json
+import boto3
+
+
+def load_stats(glue_client: boto3.client, bucket: str, key: str, filename: str) -> list[dict]:
+    """
+    load a json file containing info on one or more athena queries from an s3 location,
+    return the Statistics node for each query
+
+    Args:
+        glue_client (boto3.client): client for aws s3
+        bucket (str): name of the bucket containing the file
+        key (str): the key/location in the bucket of the file
+        filename (str): the name the file (without file extension)
+
+    Returns:
+        list[dict]: the `Statistics` for each athena query in given file
+    """
+
+    obj = glue_client.get_object(Bucket=bucket, Key=f"{key}{filename}.json")
+    results = json.loads(obj.get("Body").read())
+    stats = [q.get("Statistics") for q in results]
+
+    return stats
+
+
+def get_query_cost(stats: list[dict], cost_per_tb: float=5.0) -> dict:
+    """
+    given the statistics of a list of athena-queries return the total cost and associate metrics
+
+    Args:
+        stats (list[dict]): the `Statistics` node for one or more athena queries, as returned by
+            [GetQueryExecution](https://docs.aws.amazon.com/athena/latest/APIReference/API_GetQueryExecution.html)
+        cost_per_tb (float, optional): the cost (in dollars), per terrabyte-scanned of an athena
+            query as set by aws. Defaults to 5.0.
+
+    Returns:
+        dict: _description_
+    """
+
+    bytes_per_tb = 2**40
+
+    execution_time = sum([i["TotalExecutionTimeInMillis"] for i in stats]) / 1000
+    tb_scanned = sum([i["DataScannedInBytes"] for i in stats]) / bytes_per_tb
+    cost = tb_scanned * cost_per_tb
+
+    return {
+        "execution_time": execution_time,
+        "tb_scanned": tb_scanned,
+        "cost": cost
+    }
+
+
+def lambda_handler(event, context):
+
+
+    s3 = boto3.client("s3")
+
+    args = event.get("Arguments")
+
+    response_path =  "/compute=athena_iceberg/responses/"
+    key = f"{args['--output_key_base']}{response_path}"
+
+    stats = load_stats(
+        glue_client=s3,
+        bucket=args["--bucket"],
+        key=key,
+        filename=event["glue_job_id"]
+        )
+
+    costs = get_query_cost(stats)
+
+    return {
+        'statusCode': 200,
+        'body': {**costs,
+                 **event}
+    }

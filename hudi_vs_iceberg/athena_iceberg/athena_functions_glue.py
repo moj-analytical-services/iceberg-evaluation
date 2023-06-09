1768659ba10279d045d788154b61ea0f4ebe568c
William Orr <william.orr@digital.justice.gov.uk>
2023-06-09 11:16:21 +0100

    Delete files with more logging

--- hudi_vs_iceberg/athena_iceberg/athena_functions_glue.py
+++ hudi_vs_iceberg/athena_iceberg/athena_functions_glue.py
@@ -6,10 +6,10 @@ import sys
 import time
 
 root = logging.getLogger()
-root.setLevel(logging.INFO)
+root.setLevel(logging.DEBUG)
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
+handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 root.addHandler(handler)
@@ -17,8 +17,7 @@ root.info("check")
 
 # settings and boilerplate code can go here (if its the same for all use cases) 
 def get_job_run_id():
-    datetime_str = time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime())
-    job_run_id = f"{datetime_str}_{use_case}_{scd2_type}_{table}_{scale}_{proportion}"
+    job_run_id = f"{use_case}_{scd2_type}_{table}_{scale}_{str_proportion}"
     return job_run_id
 
 def resp_to_s3(resp, job_run_id):
@@ -40,14 +39,34 @@ def run_queries(queries, workgroup):
         else:
             return 1
     return results
+    
+def clean_dest_folder(bucket, table_dest_folder):
+    client = boto3.client('s3')
+    obj_list_resp = client.list_objects_v2(Bucket=bucket, Prefix=table_dest_folder)
+    root.info(f"""Object list from destination table folder {table_dest_folder}:
+        {obj_list_resp}""")
+    for obj in obj_list_resp['Contents']:
+        client.delete_object(Bucket=bucket, Key=obj['Key'])
+        root.info(f"Deleted {obj['Key']}")
 
-def bulk_insert(dest_database_name, dest_ice_table_name, output_data_directory, future_end_datetime, source_database_name, source_table_name):
-    create_db_sql = f"CREATE DATABASE IF NOT EXISTS {dest_database_name}"
-    drop_dest_sql = f"DROP TABLE IF EXISTS {dest_database_name}.{dest_ice_table_name}"
+def bulk_insert(bucket, dest_database_name, dest_ice_table_name, output_data_directory, future_end_datetime, source_database_name, source_table_name):
+    table_dest_folder = f"{output_data_directory}{dest_database_name}/{dest_ice_table_name}"
+    root.info(f"""Vars recieved to bulk insert:
+        bucket: {bucket}
+        dest_database_name: {dest_database_name}
+        dest_ice_table_name: {dest_ice_table_name}
+        output_data_directory: {output_data_directory}
+        future_end_datetime: {future_end_datetime}
+        source_database_name: {source_database_name}
+        source_table_name: {source_table_name}
+        table_dest_folder: {table_dest_folder}
+        """)
+    create_db_sql = f"CREATE DATABASE IF NOT EXISTS {dest_database_name};"
+    drop_dest_sql = f"DROP TABLE IF EXISTS {dest_database_name}.{dest_ice_table_name} PURGE;"
     bulk_insert_sql = f"""
         CREATE TABLE IF NOT EXISTS {dest_database_name}.{dest_ice_table_name}
         WITH (table_type='ICEBERG',
-            location='{output_data_directory}{dest_database_name}/{dest_ice_table_name}',
+            location='{table_dest_folder}',
             format='PARQUET',
             is_external=false)
         AS SELECT
@@ -83,6 +102,7 @@ def bulk_insert(dest_database_name, dest_ice_table_name, output_data_directory, 
         FROM {source_database_name}.{source_table_name} src;
     """
     job_run_id = get_job_run_id()
+    clean_dest_folder(bucket, table_dest_folder)
     queries = [create_db_sql, drop_dest_sql, bulk_insert_sql]
     results = run_queries(queries, workgroup)
     resp_to_s3(results, job_run_id)
@@ -166,7 +186,7 @@ def scd2_complex(dest_database_name, dest_ice_table_name, source_database_name, 
             ,ss_net_paid         
             ,ss_net_paid_inc_tax 
             ,ss_net_profit       
-            ,CAST(extraction_timestamp  as timestamp(6)) as extraction_timestamp
+            ,CAST(extraction_timestamp  AS TIMESTAMP(6)) as extraction_timestamp
             ,op                  
             ,pk  
             ,CAST(extraction_timestamp AS TIMESTAMP(6)) AS start_datetime
@@ -183,15 +203,16 @@ def scd2_complex(dest_database_name, dest_ice_table_name, source_database_name, 
         is_external=false)
         AS 
         WITH end_date AS (
-            SELECT {primary_key}, extraction_timestamp, LEAD(extraction_timestamp, 1, TIMESTAMP '{future_end_datetime}')
+            SELECT {primary_key}, CAST(extraction_timestamp  AS TIMESTAMP(6)) as extraction_timestamp, 
+                    LEAD(CAST(extraction_timestamp  AS TIMESTAMP(6)), 1, TIMESTAMP '{future_end_datetime}')
                 OVER (PARTITION BY {primary_key} 
             ORDER BY extraction_timestamp) AS end_datetime_lead
-            FROM {dest_database_name}.{dest_ice_table_name}
+            FROM {source_database_name}.{update_table_name}
         )
         SELECT {primary_key}, extraction_timestamp, end_datetime_lead,
             CASE WHEN end_datetime_lead = CAST(TIMESTAMP '{future_end_datetime}' AS TIMESTAMP(6)) THEN true
                 ELSE false END AS is_current 
-        FROM end_date
+        FROM end_date;
     """
     complex_merge_sql = f"""
         MERGE INTO {dest_database_name}.{dest_ice_table_name} ice
@@ -201,10 +222,11 @@ def scd2_complex(dest_database_name, dest_ice_table_name, source_database_name, 
             WHEN MATCHED 
                 THEN UPDATE
                     SET end_datetime = tmp.end_datetime_lead,
-                        is_current = tmp.is_current
+                        is_current = tmp.is_current;
     """
+    drop_dest_sql = f"DROP TABLE IF EXISTS {dest_database_name}.{dest_ice_table_name} PURGE;"
     job_run_id = get_job_run_id()
-    queries = [simple_insert_sql, drop_complex_temp_tbl_sql, create_complex_temp_tbl_sql, complex_merge_sql, drop_complex_temp_tbl_sql]
+    queries = [simple_insert_sql, drop_complex_temp_tbl_sql, create_complex_temp_tbl_sql, complex_merge_sql, drop_complex_temp_tbl_sql, drop_dest_sql]
     results = run_queries(queries, workgroup)
     resp_to_s3(results, job_run_id)
     
@@ -257,7 +279,7 @@ if __name__ == "__main__":
     
     
     if use_case == "bulk_insert":
-        _ = bulk_insert(dest_database_name, dest_ice_table_name, output_data_directory, future_end_datetime, source_database_name, source_table_name)
+        _ = bulk_insert(bucket, dest_database_name, dest_ice_table_name, output_data_directory, future_end_datetime, source_database_name, source_table_name)
     
     if use_case == "scd2_simple":
         _ = scd2_simple(dest_database_name, dest_ice_table_name, source_database_name, update_table_name, primary_key, future_end_datetime)

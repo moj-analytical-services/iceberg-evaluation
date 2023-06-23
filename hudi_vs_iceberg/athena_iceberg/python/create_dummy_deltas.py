cb036c299689f6a0b2a634918cdd3ce8be9339f6
gwionap <32736223+gwionap@users.noreply.github.com>
2023-06-23 14:49:35 +0100

    Moved python deltas example into own subdirectory and added R examples to show compatibility with analytical tools developed for the R community. (#35)

--- hudi_vs_iceberg/athena_iceberg/python/create_dummy_deltas.py
+++ hudi_vs_iceberg/athena_iceberg/python/create_dummy_deltas.py
@@ -16,15 +16,16 @@ def get_dummy_deltas(employees_path):
     for col in ["sex", "forename", "surname"]:
         employees[col] = employees[col].astype(pd.StringDtype())
 
-
     # Let's split up the data and make some changes
-    day1 = employees[employees.employee_id.isin([1,2,3,4,5])].reset_index(drop=True)
+    day1 = employees[employees.employee_id.isin([1, 2, 3, 4, 5])].reset_index(drop=True)
 
-    day2 = employees[employees.employee_id.isin([5,6,7])].reset_index(drop=True)
+    day2 = employees[employees.employee_id.isin([5, 6, 7])].reset_index(drop=True)
     day2.loc[0, "department_id"] = 2
     day2.loc[0, "manager_id"] = 18
 
-    day3 = employees[employees.employee_id.isin([1,7,9,10,11])].reset_index(drop=True)
+    day3 = employees[employees.employee_id.isin([1, 7, 9, 10, 11])].reset_index(
+        drop=True
+    )
     day3.department_id = 2
     day3.manager_id = 5
 
@@ -32,11 +33,7 @@ def get_dummy_deltas(employees_path):
     day3.loc[0, "record_deleted"] = True
     day3.loc[0, "department_id"] = 1
     day3.loc[0, "manager_id"] = 17
-    
-    deltas = {
-        "day1": day1,
-        "day2": day2,
-        "day3": day3
-    }
+
+    deltas = {"day1": day1, "day2": day2, "day3": day3}
 
     return deltas

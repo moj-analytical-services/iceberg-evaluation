7919ebfa5734ce1451c3d14a5c91b03e97d18e19
gwionap <32736223+gwionap@users.noreply.github.com>
2023-06-23 14:50:30 +0100

    SQL benchmarking for TPCDS partitioned at scale=3000 (#34)
    * Initial benchmarking functions.
    
    * Initial query benchmarking notebook with associated utils and stats functions.
    
    * Remove warning.
    
    * Notebook for scale=3000 partitioned databases.
    
    * Added 1. benchmark results for partitioned TPCDS at scale=3000 for hive and iceberg; 2. simple script for inferring metadata for the partitioned 3000 scale hive TPCDS data from the scale=1 data; 3. upades to the iceberg utility script to handle inserts of partitions from a partitioned hive TPCDS dataset to an iceberg table.

--- hudi_vs_iceberg/query_performance/utils/create_iceberg_tpcds.py
+++ hudi_vs_iceberg/query_performance/utils/create_iceberg_tpcds.py
@@ -1,4 +1,5 @@
 import os
+from itertools import chain
 
 import awswrangler as wr
 from jinja2 import Template
@@ -7,8 +8,10 @@ os.environ["AWS_DEFAULT_REGION"] = "eu-west-1"
 
 wr.config.workgroup = "AthenaWorkGroupIceberg"
 
-database_name = "tpcds_1"
-iceberg_database_name = "tcpds_1_iceberg"
+database_name = "tpcds_3000_partitioned"
+iceberg_database_name = "tpcds_iceberg_3000_partitioned"
+current_prefix = "/BLOG_TPCDS-TEST-3T-partitioned/"
+new_prefix = "/BLOG_TPCDS-TEST-3T-partitioned-iceberg/"
 
 tables = wr.catalog.get_tables(database=database_name)
 
@@ -21,6 +24,13 @@ create_sql_template = Template(
         {{ column["Name"] }} {{ column["Type"] }}{{ ", " if not loop.last else "" }}
     {% endfor %}
     )
+    {% if partitions %}
+        PARTITIONED BY (
+            {% for partition in partitions %}
+                {{ partition }}{{ ", " if not loop.last else "" }}
+            {% endfor %}
+        )
+    {% endif %}
     LOCATION '{{ table_path }}'
     TBLPROPERTIES (
         'table_type'='ICEBERG',
@@ -33,16 +43,27 @@ insert_sql_template = Template(
     """
     INSERT INTO {{ iceberg_database_name }}.{{ table_name }}
     SELECT * FROM {{ database_name }}.{{ table_name }}
+    {% if partitions %}
+        WHERE {{ partition_key }} IN (
+            {% for partition in partitions %}
+                {{ partition }}{{ ", " if not loop.last else "" }}
+            {% endfor %}
+        )
+    {% endif %}
     """
 )
 
 for table in tables:
+    print(f"Creating iceberg table for {table}")
+
+    table_partitions = [p["Name"] for p in table["PartitionKeys"]]
     create_template_data = {
         "database_name": iceberg_database_name,
         "table_name": table["Name"],
-        "columns": table["StorageDescriptor"]["Columns"],
+        "columns": table["StorageDescriptor"]["Columns"] + table["PartitionKeys"],
+        "partitions": table_partitions,
         "table_path": table["StorageDescriptor"]["Location"].replace(
-            "/tpcds/", "/tpcds_iceberg/"
+            current_prefix, new_prefix
         ),
     }
 
@@ -52,10 +73,32 @@ for table in tables:
     )
     create_resp = wr.athena.wait_query(query_execution_id=create_id)
 
-    insert_sql = insert_sql_template.render(
-        database_name=database_name,
-        iceberg_database_name=iceberg_database_name,
-        table_name=table["Name"],
-    )
-    insert_id = wr.athena.start_query_execution(insert_sql)
-    insert_resp = wr.athena.wait_query(insert_id)
+    if len(table_partitions) == 1:
+        loaded_partitions = wr.catalog.get_partitions(database_name, table["Name"])
+        updated_partitions = list(chain(*[p for _, p in loaded_partitions.items()]))
+        updated_partitions = [
+            p if p.lower() != "__hive_default_partition__" else "NULL"
+            for p in updated_partitions
+        ]
+        step = 100
+        for i in range(100, len(updated_partitions), step):
+            print(f"Inserting partitions {i} to {i + step}")
+            current_partitions = updated_partitions[i : i + step]
+            insert_sql = insert_sql_template.render(
+                database_name=database_name,
+                iceberg_database_name=iceberg_database_name,
+                table_name=table["Name"],
+                partition_key=table_partitions[0],
+                partitions=current_partitions,
+            )
+            insert_id = wr.athena.start_query_execution(insert_sql)
+            insert_resp = wr.athena.wait_query(insert_id)
+
+    else:
+        insert_sql = insert_sql_template.render(
+            database_name=database_name,
+            iceberg_database_name=iceberg_database_name,
+            table_name=table["Name"],
+        )
+        insert_id = wr.athena.start_query_execution(insert_sql)
+        insert_resp = wr.athena.wait_query(insert_id)

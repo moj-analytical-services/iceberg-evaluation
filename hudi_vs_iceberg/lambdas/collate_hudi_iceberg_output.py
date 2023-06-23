5cda533be9d2122de331486f1b4332ca20ee7e11
David Bridgwood <124149420+davidbridgwood@users.noreply.github.com>
2023-06-23 11:34:00 +0100

    add state machine definition (#36)
    * add lambdas to calculate costs
    
    * update docstrings
    
    * change filename structure for athena-query-costs
    
    * lambda for collecting all results
    
    * update to accomidate for changed output - each compute now runs in separate parallel state
    
    * add state machine definition

---
+++ hudi_vs_iceberg/lambdas/collate_hudi_iceberg_output.py
@@ -0,0 +1,81 @@
+import datetime
+import json
+import boto3
+
+
+def flatten_dict(d: dict) -> dict:
+    """
+    where values in a dict are a dict themselve bring them up a level
+    works recursively to create a 'flat' dictionary
+
+    note:
+        - keys where the value is a dictionary will be lost
+        - where keys are repeated in a nested dict, the deepest value will be the one which is
+          preserved
+    """
+    o = dict()
+    for k, v in d.items():
+        if isinstance(v, dict):
+            o = {**o,
+                 **flatten_dict(v)}
+        else:
+            o[k] = v
+    return o
+
+
+def flatten_list(nested):
+    """recursively flatten a nested list"""
+    if not isinstance(nested, list):
+        return [nested]
+
+    result = []
+    for i in nested:
+        result.extend(flatten_list(i))
+    return result
+
+
+def format_dict(d: dict) -> dict:
+    """flatten dict and remove '--' prefix from keys"""
+    if d:
+        d = flatten_dict(d)
+        d = {k.lstrip("--"): v for k, v in d.items()}
+    return d
+
+
+def collate_format_output(output: dict) -> list[dict]:
+    """
+    takes the output of the hudi_vs_iceberg state-machine and returns a list of dicts with the
+    results for each update proportion, compute and use-case tested
+    """
+    # these are the potential steps we want to measure
+    keys = ["bulk_insert_simple", "bulk_insert_complex", "simple_scd2", "scd2_complex"]
+
+    test_computes = flatten_list([pr["test_computes"] for pr in output["proportion_results"]])
+
+    results = []
+    for compute in test_computes:
+        results.extend([format_dict(compute.get(k)) for k in keys])
+
+    results = [r for r in results if r]
+    return results
+
+
+def lambda_handler(event, context):
+    results = collate_format_output(event)
+
+    bucket = event["bucket"]
+    output_key_base = event["output_key_base"]
+    timestamp = datetime.datetime.now()
+    filename = f"hudi_vs_iceberg_{timestamp.strftime('%Y%m%d%H%M%S')}.json"
+    key = f"{output_key_base}/step_function_results/{filename}"
+
+    s3 = boto3.client("s3")
+
+    s3.put_object(Body=json.dumps(results),
+                  Bucket=bucket,
+                  Key=key)
+
+    return {
+        'statusCode': 200,
+        'body': results
+    }

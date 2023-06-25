c7baef2a8ad16b37a6e9c3be0aac5822afbe113c
SoumayaMauthoorMOJ <soumaya.mauthoor@digital.justice.gov.uk>
2023-06-25 13:47:46 +0100

    update tpcds code

--- hudi_vs_iceberg/tpcds_generator/generate_tpcds.py
+++ hudi_vs_iceberg/tpcds_generator/generate_tpcds.py
@@ -2,35 +2,40 @@ import boto3
 import sys
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
+from awsglue.utils import getResolvedOptions
 import pyspark.sql.functions as f
 from awsglue.dynamicframe import DynamicFrame
 from pyspark.context import SparkConf
 
 
-def _getOptionalResolvedOptions(arguments):
-    args_dict = {}
-    for i in range(1, len(arguments), 2):
-        arg_name = arguments[i].lstrip("--")
-        arg_value = arguments[i + 1]
-        args_dict[arg_name] = arg_value
-    return args_dict
+args = getResolvedOptions(
+    sys.argv,
+    [
+        "bucket",
+        "table",
+        "scale",
+        "num_workers",
+        "table_format",
+    ],
+)
 
-
-args = _getOptionalResolvedOptions(sys.argv)
 
 bucket_name = args.get("bucket", "sb-test-bucket-ireland")
 table_filter = args.get("table", "store_sales")
 scale = args.get("scale", 1)
-table_format = args.get("table_format", "")  # options: "_iceberg"  "_hudi". "" for hive
-partitions = 36  # 396
+# options: "iceberg" , "hudi" , "hive"
+table_format = args.get("table_format", "hive")
+num_workers = int(args.get("num_workers", 10))
+partitions = (num_workers - 1) * 4
 extraction_timestamp = "2022-01-01"
-database_name = f"tpcds{table_format}_{scale}"
+table_format_suffix = ""
 
-if table_format == "":
+
+if table_format == "hive":
     glue_context = GlueContext(SparkContext())
     spark = glue_context.spark_session
 
-if table_format == "_hudi":
+if table_format == "hudi":
     conf = SparkConf()
     conf.set("spark.default.parallelism", partitions)
     conf.set("spark.sql.shuffle.partitions", partitions)
@@ -38,6 +43,29 @@ if table_format == "_hudi":
     conf.set("spark.sql.hive.convertMetastoreParquet", "false")
     glue_context = GlueContext(SparkContext.getOrCreate(conf=conf))
     spark = glue_context.spark_session
+    table_format_suffix = "_hudi"
+
+if table_format == "iceberg":
+    conf = SparkConf()
+    conf.set("spark.sql.catalog.glue_catalog", "org.apache.iceberg.spark.SparkCatalog")
+    conf.set(
+        "spark.sql.catalog.glue_catalog.warehouse",
+        f"s3://{bucket_name}/tpcds{table_format_suffix}/scale={scale}",
+    )
+    conf.set(
+        "spark.sql.catalog.glue_catalog.catalog-impl",
+        "org.apache.iceberg.aws.glue.GlueCatalog",
+    )
+    conf.set(
+        "spark.sql.catalog.glue_catalog.io-impl", "org.apache.iceberg.aws.s3.S3FileIO"
+    )
+    conf.set(
+        "spark.sql.extensions",
+        "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
+    )
+    glue_context = GlueContext(SparkContext.getOrCreate(conf=conf))
+    spark = glue_context.spark_session
+    table_format_suffix = "_iceberg"
 
 tables = {
     "call_center": "cc_call_center_sk",
@@ -69,6 +97,7 @@ tables = {
 if table_filter:
     tables = {table_filter: tables[table_filter]}
 
+database_name = f"tpcds{table_format_suffix}_{scale}"
 ## Create a database if not exists
 try:
     glue = boto3.client("glue")
@@ -79,12 +108,13 @@ except glue.exceptions.AlreadyExistsException:
 
 
 for table, primary_key in tables.items():
-    path = f"tpcds{table_format}/scale={scale}/table={table}/"
+    table_prefix = f"tpcds{table_format_suffix}/scale={scale}/table={table}/"
+    table_path = f"s3://{bucket_name}/{table_prefix}"
 
     ## Delete files in S3
     s3 = boto3.resource("s3")
     bucket = s3.Bucket(bucket_name)
-    bucket.objects.filter(Prefix=path).delete()
+    bucket.objects.filter(Prefix=table_prefix).delete()
 
     ## Drop table in Glue Data Catalog
     try:
@@ -112,14 +142,28 @@ for table, primary_key in tables.items():
     df = df.withColumn("op", f.lit(None).cast("string"))
 
     if isinstance(primary_key, list):
-        df = df.withColumn("pk", f.md5(f.concat(*primary_key)))
+        df = df.withColumn("pk", f.md5(f.concat_ws("-", *primary_key)))
         primary_key = "pk"
 
-    if table_format == "":
+    if table_format == "hive":
         dyf2 = DynamicFrame.fromDF(df, glue_context, "dyf2")
+        
+        # To resize files, currently commented out:
+        # logger = glue_context.get_logger()
+        # average_row_size = df.rdd.map(lambda x: len(str(x))).mean()
+        # logger.warn(f"average_row_size = {str(average_row_size)}")
+        # # Define your desired file size : 800 is a bit of a magic number but it works
+        # desired_file_size = 800 * 1024 * 1024  # in bytes, 1MB
+        # logger.warn(f"desired_file_size in bytes = {str(desired_file_size)}")
+        # records_per_file = max(int(desired_file_size / average_row_size), 1)
+        # logger.warn(f"records_per_file = {str(records_per_file)}")
+        # total_rows = df.count()
+        # no_partitions = int(total_rows / records_per_file)
+        # dyf2 = dyf2.repartition(no_partitions)
+        
         sink = glue_context.getSink(
             connection_type="s3",
-            path=f"s3://{bucket_name}/{path}",
+            path=table_path,
             enableUpdateCatalog=True,
             updateBehavior="UPDATE_IN_DATABASE",
         )
@@ -127,19 +171,19 @@ for table, primary_key in tables.items():
         sink.setCatalogInfo(catalogDatabase=database_name, catalogTableName=table)
         sink.writeFrame(dyf2)
 
-    if table_format == "_hudi":
+    if table_format == "hudi":
         hudi_options = {
             "hoodie.table.name": table,
             "hoodie.datasource.write.storage.type": "COPY_ON_WRITE",
             "hoodie.datasource.write.recordkey.field": primary_key,
             #'hoodie.datasource.write.partitionpath.field': 'extraction_timestamp',
             "hoodie.datasource.write.table.name": table,
-            "hoodie.datasource.write.operation": "upsert",
+            "hoodie.datasource.write.operation": "bulk_insert",
             "hoodie.datasource.write.precombine.field": "extraction_timestamp",
             "hoodie.datasource.write.hive_style_partitioning": "true",
             "hoodie.upsert.shuffle.parallelism": partitions,
             "hoodie.insert.shuffle.parallelism": partitions,
-            "path": f"s3://{bucket_name}/{path}",
+            "path": table_path,
             "hoodie.datasource.hive_sync.enable": "true",
             "hoodie.datasource.hive_sync.database": database_name,
             "hoodie.datasource.hive_sync.table": table,
@@ -150,3 +194,17 @@ for table, primary_key in tables.items():
         }
 
         df.write.format("hudi").options(**hudi_options).mode("overwrite").save()
+
+    if table_format == "iceberg":
+        # Create an Iceberg table from a DataFrame
+        # and register the table to Glue Data Catalog
+        df.createOrReplaceTempView(f"tmp_{table}")
+
+        query = f"""
+        CREATE TABLE glue_catalog.{database_name}.{table}
+        USING iceberg
+        LOCATION '{table_path}'
+        AS SELECT * FROM tmp_{table}
+        """
+
+        spark.sql(query)
--- hudi_vs_iceberg/tpcds_generator/update_tpcds.py
+++ hudi_vs_iceberg/tpcds_generator/update_tpcds.py
@@ -9,32 +9,37 @@ from awsglue.dynamicframe import DynamicFrame
 glue_context = GlueContext(SparkContext())
 spark = glue_context.spark_session
 
-
-def _getOptionalResolvedOptions(arguments):
-    args_dict = {}
-    for i in range(1, len(arguments), 2):
-        arg_name = arguments[i].lstrip("--")
-        arg_value = arguments[i + 1]
-        args_dict[arg_name] = arg_value
-    return args_dict
-
-
-args = _getOptionalResolvedOptions(sys.argv)
+args = getResolvedOptions(
+    sys.argv,
+    [
+        "bucket",
+        "table",
+        "scale",
+        "proportion",
+    ],
+)
 
 bucket_name = args.get("bucket", "sb-test-bucket-ireland")
 source_table = args.get("table", "store_sales")
 scale = args.get("scale", 1)
-proportion = args.get("proportion", 0.001)
+proportion = float(args.get("proportion", 0.001))
+
 str_proportion = str(proportion).replace(".", "_")
 extraction_timestamp = "2022-01-02"
 database_name = f"tpcds_{scale}"
 table = f"{source_table}_{str_proportion}"
-path = f"tpcds_updates/scale={scale}/table={source_table}/proportion={str_proportion}/"
+table_prefix = f"tpcds_updates/scale={scale}/table={source_table}/proportion={str_proportion}/"
+
+logger = glue_context.get_logger()
+logger.warn(f"{str(args)}")
+logger.warn(f"{table=}")
+logger.warn(f"{table_prefix=}")
+
 
 ## Delete files in S3
 s3 = boto3.resource("s3")
 bucket = s3.Bucket(bucket_name)
-bucket.objects.filter(Prefix=path).delete()
+bucket.objects.filter(Prefix=table_prefix).delete()
 
 ## Drop table in Glue Data Catalog
 try:
@@ -48,7 +53,7 @@ df = glue_context.create_data_frame.from_catalog(
     table_name=source_table,
     additional_options={"useCatalogSchema": True},
 )
-df = df.sample(False, fraction=0.001, seed=1)
+df = df.sample(False, fraction=proportion, seed=1)
 df = df.withColumn(
     "extraction_timestamp",
     f.to_timestamp(f.lit(extraction_timestamp), "yyyy-MM-dd"),
@@ -61,7 +66,7 @@ dyf2 = DynamicFrame.fromDF(df, glue_context, "dyf2")
 # Write the DynamicFrame to S3 and register the table.
 sink = glue_context.getSink(
     connection_type="s3",
-    path=f"s3://{bucket_name}/{path}",
+    path=f"s3://{bucket_name}/{table_prefix}",
     enableUpdateCatalog=True,
     updateBehavior="UPDATE_IN_DATABASE",
 )

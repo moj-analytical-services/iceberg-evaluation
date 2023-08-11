33e697448c9c7ddb48513b3353146df39a6e453e
sivabathina2 <123939271+sivabathina2@users.noreply.github.com>
2023-08-11 16:29:34 +0100

    Test pyspark functions (#53)
    * testing pyspark functions
    
    * working version of pytest
    
    * working version of pytest

--- src/data_curation/tests/test_pyspark_functions.py
+++ src/data_curation/tests/test_pyspark_functions.py
@@ -1,3 +1,4 @@
+
 import pytest
 import sys
 import pandas as pd
@@ -8,14 +9,58 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkConf
 from glue_jobs.pyspark_functions import * 
 from tests.compare_df import assert_df_equality
+import datetime
+
+from pyspark.sql import SparkSession, functions as F
+from pyspark.sql import SparkSession
+
+from pyspark.sql import SparkSession, functions as F
+from pyspark.sql.types import StructType, StructField, IntegerType, Row
+from pyspark.context import SparkConf
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.utils import getResolvedOptions
+import time
+
+
 
 bucket_name = "sb-test-bucket-ireland"
 key = "tpcds_test"
-directory = f"s3://{bucket_name}/{key}"
 compute = "glue_iceberg"
 database_name = "tpcds_test"
+bucket_prefix = "sb"
+table_name = "datagensb"
+warehouse_path = f"s3://{bucket_name}/{bucket_prefix}"
+directory = f"s3://{bucket_name}/{key}"
 
-@pytest.fixture(scope="module", autouse=True)
+
+catalog_name = "glue_catalog"
+bucket_name = "sb-test-bucket-ireland"
+bucket_prefix = "sb"
+table_name = "datagensb"
+warehouse_path = f"s3://{bucket_name}/{bucket_prefix}"
+input_prefix = "tpcds_test"
+input_path = f"s3://{bucket_name}/{input_prefix}"
+source_database_name = "tpcds_test"
+dest_database_name = "tpcds_test_glue_iceberg"
+output_directory = f"{catalog_name}.{dest_database_name}.{table_name}"
+future_end_datetime = datetime.datetime(2250, 1, 1)
+
+
+spark = SparkSession.builder \
+    .config(f"spark.sql.catalog.{catalog_name}", "org.apache.iceberg.spark.SparkCatalog") \
+    .config(f"spark.sql.catalog.{catalog_name}.warehouse", f"{warehouse_path}") \
+    .config(f"spark.sql.catalog.{catalog_name}.catalog-impl", "org.apache.iceberg.aws.glue.GlueCatalog") \
+    .config(f"spark.sql.catalog.{catalog_name}.io-impl", "org.apache.iceberg.aws.s3.S3FileIO") \
+    .config("spark.sql.extensions","org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
+    .getOrCreate()
+
+
+
+
+
+
+@pytest.fixture(scope="module")
 def glue_context():
     sys.argv.append('--JOB_NAME')
     sys.argv.append('test_count')
@@ -33,22 +78,53 @@ def glue_context():
     job.commit()
 
 
-def test_bulk_insert(glue_context):
-    actual_df = pd.read_parquet(f"{directory}/bulk_insert")
-    expected_df = pd.read_parquet(f"{directory}/bulk_insert")
+@pytest.fixture(scope="session")
+def spark():
+    spark = (
+        SparkSession.builder.config(
+            f"spark.sql.catalog.{catalog_name}", "org.apache.iceberg.spark.SparkCatalog"
+        )
+        .config(f"spark.sql.catalog.{catalog_name}.warehouse", f"{warehouse_path}")
+        .config(
+            f"spark.sql.catalog.{catalog_name}.catalog-impl",
+            "org.apache.iceberg.aws.glue.GlueCatalog",
+        )
+        .config(
+            f"spark.sql.catalog.{catalog_name}.io-impl",
+            "org.apache.iceberg.aws.s3.S3FileIO",
+        )
+        .config(
+            "spark.sql.extensions",
+            "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
+        )
+        .getOrCreate()
+    )
+    yield spark
+    #spark.stop()
+
+   
+
+def test_bulk_insert(glue_context,spark):
+
+    bulk_insert(f"{input_path}/full_load", output_directory, future_end_datetime,spark)
+    actual_df = pd.read_parquet(f"s3://{bucket_name}/{input_prefix}/bulk_insert")
+    expected_df = spark.table(output_directory).toPandas()
     assert_df_equality(expected_df,actual_df)
     
-def test_scd2_simple_update(glue_context):
-    actual_df = pd.read_parquet(f"{directory}/update_1")
-    expected_df = pd.read_parquet(f"{directory}/update_1")
+def test_scd2_simple_update(glue_context,spark):
+    scd2_simple(f"{input_path}/cdc_1", output_directory, future_end_datetime, "pk",spark)
+    actual_df = pd.read_parquet(f"s3://{bucket_name}/{input_prefix}/update_1")
+    expected_df = spark.table(output_directory).toPandas()
     assert_df_equality(expected_df,actual_df)
     
-def test_scd2_simple_insert(glue_context):
-    actual_df = pd.read_parquet(f"{directory}/update_2")
-    expected_df = pd.read_parquet(f"{directory}/update_2")
+def test_scd2_simple_insert(glue_context,spark):
+    scd2_simple(f"{input_path}/cdc_2", output_directory, future_end_datetime, "pk",spark)
+    actual_df = pd.read_parquet(f"s3://{bucket_name}/{input_prefix}/update_2")
+    expected_df = spark.table(output_directory).toPandas()
     assert_df_equality(expected_df,actual_df)
     
-def test_scd2_complex(glue_context):
-    actual_df = pd.read_parquet(f"{directory}/update_3")
-    expected_df = pd.read_parquet(f"{directory}/update_3")
+def test_scd2_complex(glue_context,spark):
+    scd2_complex(f"{input_path}/cdc_3", output_directory, future_end_datetime, "pk",spark)
+    actual_df = pd.read_parquet(f"s3://{bucket_name}/{input_prefix}/update_3")
+    expected_df = spark.table(output_directory).toPandas()
     assert_df_equality(expected_df,actual_df)
